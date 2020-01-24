dbd1b9432a9816cec126495da74d687ad508da30
faellfurttado <46831447+faellfurttado@users.noreply.github.com>
2020-01-24 15:47:54 -0300

    Add files via upload

---
+++ map_discretized_list/src/main/java/br/edu/ifma/pesquisa/map_discretized_list/AssignSequences.java
@@ -0,0 +1,12 @@
+package br.edu.ifma.pesquisa.map_discretized_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignSequences {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignSequences.class, args);
+	}
+}
