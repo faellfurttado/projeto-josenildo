b8393855895c122746d2bf42226ac3599cfb056e
Jorge Luis L Amorim <lopesluisjorge@hotmail.com>
2020-09-07 16:55:09 -0300

    Iniciando o frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="AppHeader"></header>
+      <main className="AppMain"></main>
+      <footer className="AppFooter"></footer>
+    </div>
+  );
+}
+
+export default App;
